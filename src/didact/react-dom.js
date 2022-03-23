82e95ee84183cf26e9978f8c7458e3e4823003ab
Marvin <uuuhds@outlook.com>
2022-03-23 15:01:14 +0800

    add Function Component render

--- src/didact/react-dom.js
+++ src/didact/react-dom.js
@@ -56,17 +56,12 @@ function workLoop(deadLine) {
 requestIdleCallback(workLoop);
 
 function performUnitOfWork(fiber) {
-  if (!fiber.dom) {
-    fiber.dom = createDom(fiber);
+  const isFunctionComponent = fiber.type instanceof Function;
+  if (isFunctionComponent) {
+    updateFunctionComponent(fiber);
+  } else {
+    updateHostComponent(fiber);
   }
-
-  // if (fiber.parent) {
-  //   fiber.parent.dom.appendChild(fiber.dom);
-  // }
-
-  const elements = fiber.props.children;
-
-  reconcileChildren(fiber, elements);
 
   if (fiber.child) {
     return fiber.child;
@@ -79,6 +74,19 @@ function performUnitOfWork(fiber) {
     }
     nextFiber = nextFiber.parent;
   }
+}
+
+function updateFunctionComponent(fiber) {
+  // function App() {}  App(props) => JSX.Element
+  const children = [fiber.type(fiber.props)];
+  reconcileChildren(fiber, children);
+}
+
+function updateHostComponent(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  reconcileChildren(fiber, fiber.props.children);
 }
 
 function reconcileChildren(wipFiber, elements) {
@@ -139,7 +147,7 @@ function reconcileChildren(wipFiber, elements) {
 }
 
 function commitRoot() {
-  console.log('deletions', deletions)
+  console.log("deletions", deletions);
   deletions.forEach(commitWork);
   commitWork(wipRoot.child);
   currentRoot = wipRoot;
@@ -150,20 +158,28 @@ function commitWork(fiber) {
   if (!fiber) {
     return;
   }
-  // let domParentFiber = fiber.parent;
-  // while (!domParentFiber.dom) {
-  //   domParentFiber = domParentFiber.parent;
-  // }
-  const domParent = fiber.parent.dom;
+  let domParentFiber = fiber.parent;
+  while (!domParentFiber.dom) {
+    domParentFiber = domParentFiber.parent;
+  }
+  const domParent = domParentFiber.dom;
   if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
     domParent.appendChild(fiber.dom);
   } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
     updateDom(fiber.dom, fiber.alternate.props, fiber.props);
   } else if (fiber.effectTag === "DELETION") {
-    domParent.removeChild(fiber.dom);
+    commitDeletion(fiber, domParent);
   }
   commitWork(fiber.child);
   commitWork(fiber.sibling);
+}
+
+function commitDeletion(fiber, domParent) {
+  if (fiber.dom) {
+    domParent.removeChild(fiber.dom);
+  } else {
+    commitDeletion(fiber.child, domParent);
+  }
 }
 
 const isProperty = (key) => key !== "children";
--- src/index.js
+++ src/index.js
@@ -1,27 +1,9 @@
 import React from "./didact/react";
 import ReactDOM from "./didact/react-dom";
 
-ReactDOM.render(
-  <div
-    onClick={() => {
-      console.log(1);
-    }}
-  >
-    <div>foo</div>
-    <div>bar</div>
-  </div>,
-  document.getElementById("root")
-);
+function App(props) {
+  return <h1>Hi {props.name}</h1>;
+}
+const element = <App name="foo" />;
 
-setTimeout(() => {
-  ReactDOM.render(
-    <div
-      onClick={() => {
-        console.log(2);
-      }}
-    >
-      <div>boo</div>
-    </div>,
-    document.getElementById("root")
-  );
-}, 2000);
+ReactDOM.render(element, document.getElementById("root"));
